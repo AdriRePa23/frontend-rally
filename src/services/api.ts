dc385b557d85f1f66367b2a7c1fd694f7d34a02c
Adrian Real <adrianreal234@gmail.com>
2025-05-26 12:15:42 +0200

    Actualizar la URL base de la API a la dirección del servidor de producción

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api", 
+    baseURL: "http://51.21.180.82:3000/api", 
 });
 
 // Agregar token de autenticación a las solicitudes
